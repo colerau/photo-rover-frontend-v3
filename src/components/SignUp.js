f4e0b357c40f55f986d9042463d0720739b711fa
Cole Rau <colevrau@gmail.com>
2020-10-21 20:41:10 -0700

    added and styled the words sign up for the sign up form

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -18,6 +18,8 @@ class SignUp extends Component {
     return (
       <div>
         <form onSubmit={this.handleSubmit}>
+          <label className="cool-big-font">Sign Up</label>
+          <br />
           <label className="cool-font">Username:</label> 
           <input type="text" value={this.state.username} onChange={this.handleChange} className="cool-box"/>
           <input type="submit" className="cool-button"/>

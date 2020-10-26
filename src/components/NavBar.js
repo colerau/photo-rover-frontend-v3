ef4515b6f07f76831eca96f23f04456e0c4731ee
Cole Rau <colevrau@gmail.com>
2020-10-26 02:16:50 -0700

    styled navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,10 +4,10 @@ import {Link} from 'react-router-dom'
 const NavBar = props => {
   return (
     <div>
-      <Link to="/" className="App-link" style={{paddingRight: "50px"}}><img src="https://pngimg.com/uploads/mars_planet/mars_planet_PNG23.png" alt="Home" style={{height: "60px"}}/></Link>
-      <Link to="/signup" className="App-link" style={{paddingRight: "50px"}}>Sign Up</Link>
-      <Link to="/login" className="App-link" style={{paddingRight: "50px"}}>Log In</Link>
+      <Link to="/" className="App-link" style={{paddingRight: "950px"}}><img src="https://pngimg.com/uploads/mars_planet/mars_planet_PNG23.png" alt="Home" style={{height: "60px"}}/></Link>
       <Link to="/new-earth-date" className="App-link" style={{paddingRight: "50px"}}>More Photos</Link>
+      <Link to="/signup" className="App-link" style={{paddingRight: "50px"}}>Sign Up</Link>
+      <Link to="/login" className="App-link">Log In</Link>
         
     </div>
   )
